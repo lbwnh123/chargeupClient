5a92091c00187df8afadd0bf498a71c0a727b34b
liu bowen <Topoto@liudemac-mini.local>
2016-08-15 09:02:30 +0800

    Initial Commit

---
+++ NewTransport/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTransport
+//
+//  Created by Topptp on 16/8/15.
+//  Copyright © 2016年 Topoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewTransport/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewTransport
+//
+//  Created by Topptp on 16/8/15.
+//  Copyright © 2016年 Topoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
